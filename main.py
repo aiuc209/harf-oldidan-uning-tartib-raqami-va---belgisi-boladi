dc02dbcaca97c0b0e446b9b8f297f5a025b9610f
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:26:41 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Harf Oldidan Uning Tartib Raqami Va   Belgisi Boladi
+Topic: * Harf oldidan uning **tartib raqami** va `-` belgisi bo‘ladi.
+"""
+
+def main():
+    print("Welcome to Harf Oldidan Uning Tartib Raqami Va   Belgisi Boladi!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
